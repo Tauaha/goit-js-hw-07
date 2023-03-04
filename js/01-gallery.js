dab39c8cb8c517a910c9031438c51eaa8ff4048b
Tauaha <114925740+Tauaha@users.noreply.github.com>
2023-03-04 11:08:10 +0200

    task-02-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-// const galleryEl = document.querySelector('div.gallery');
-// const imagesElements = [];
-
-// galleryItems.forEach(imagesElement => {
-
-// })
 const galleryEl = document.querySelector('div.gallery');
 const imagesElements = createGalleryImgElements(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', imagesElements);
+
+
 console.log(createGalleryImgElements(galleryItems));
 function createGalleryImgElements (galleryItems){
     return galleryItems.map(({preview, original, description}) =>{
@@ -32,17 +26,18 @@ function createGalleryImgElements (galleryItems){
 galleryEl.addEventListener('click', zoomImageHandler);
 
 function zoomImageHandler(event){
-    
-// console.log(event.target.dataset.source);
-   
 if (event.target.nodeName !== 'IMG') {
     return;
 }else {
     event.preventDefault();
     const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="1400" height="900">
+    <img src="${event.target.dataset.source}" width="1280" height="900">
 `);
 
-instance.show();
-}
-}
+ instance.show();
+ window.addEventListener('keydown', closeModalHandler);
+function closeModalHandler(event){
+  instance.close();
+}}};
+
+
